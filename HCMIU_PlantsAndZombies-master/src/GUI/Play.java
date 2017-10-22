5007a4eea048a4d91cc29a000f6aefc5e0d6c65d
DuongVu98 <duongvutung1998@gmail.com>
2017-10-22 22:41:51 +0700

    render() vẽ bullet và update() thêm nút để bắn

--- HCMIU_PlantsAndZombies-master/src/GUI/Play.java
+++ HCMIU_PlantsAndZombies-master/src/GUI/Play.java
@@ -1,37 +1,69 @@
 package GUI;
+import Characters.*;
 
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
-public class Play extends BasicGameState {
-	
+public class Play extends BasicGameState
+{	
+	BulletsControl controller=new BulletsControl();
+	Zombies zombie= new Zombies();
+	Plants shooter=new Plants();
 	Image small;
-	int smallX=200;
-	int smallY=200;
     
-	
-	public Play (int state) {
+	public Play (int state)
+	{
 		
 	}
 	
 	
-	public void init(GameContainer gc, StateBasedGame sbg ) throws SlickException {
+	public void init(GameContainer gc, StateBasedGame sbg ) throws SlickException
+	{
 		 small = new Image ("res/f.png");
 	}
 	
-	public void render (GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
+	public void render (GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException
+	{
 		//g.drawString("this is the game state", 50, 50);
+		
+		controller.render(g);                     // draw bullets
+		
+		g.setColor(Color.red);
+		g.fillRect(zombie.xPos, zombie.yPos, 30, 40);    //draw zombies
+		
 		g.drawImage(small,smallX,smallY);
 		
 	}
-	public void update (GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
+	public void update (GameContainer gc, StateBasedGame sbg, int delta) throws SlickException
+	{
 		Input input = gc.getInput();
-		if (input.isKeyDown(Input.KEY_RIGHT)) {smallX +=1;}
-		if (input.isKeyDown(Input.KEY_LEFT)) {smallX -=1;}
-		if (input.isKeyDown(Input.KEY_UP)) {smallY -=1;}
-		if (input.isKeyDown(Input.KEY_DOWN)) {smallY +=1;}
+		
+		if (input.isKeyDown(Input.KEY_RIGHT)) 
+		{
+			shooter.xPos+=shooter.speed;
+		}
+		else if (input.isKeyDown(Input.KEY_LEFT)) 
+		{
+			shooter.xPos -=shooter.speed;
+		}
+		else if (input.isKeyDown(Input.KEY_UP)) 
+		{
+			shooter.yPos -=shooter.speed;
+		}
+		else if (input.isKeyDown(Input.KEY_DOWN)) 
+		{
+			shooter.yPos +=shooter.speed;
+		}
+		else if(input.isKeyPressed(Input.KEY_SPACE))                         // press space to shoot
+		{
+			controller.addBullet(new Bullet(shooter.xPos,shooter.yPos));     // bullets fly from plant position
+		}
+		
+		controller.shoot();
 	}
-	public int getID() {
-         return 1;	
+	
+	public int getID()
+	{
+        	return 1;	
 	}
 }
