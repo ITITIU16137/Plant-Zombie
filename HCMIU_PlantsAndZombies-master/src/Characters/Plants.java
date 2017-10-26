6f3a769a2e1c9d2726fcf4eda26182a3f1e2c067
ITITIU16137 <trungnguyen10051998@gmail.com>
2017-10-26 20:00:56 +0700

    Chuyển project ra ngoài

--- HCMIU_PlantsAndZombies-master/src/Characters/Plants.java
+++
@@ -1,9 +0,0 @@
-package Characters;
-
-public class Plants 
-{
-	public int xPos=200, yPos=200;
-	public int speed =1;
-	int hp;
-	int damage;
-}
---
+++ src/Characters/Bullet.java
@@ -0,0 +1,39 @@
+package Characters;
+
+
+//import org.newdawn.slick.Color;
+import org.newdawn.slick.*;
+
+public class Bullet{
+	
+	public int dX=40,dY=40;       //size of bullet
+	public double xPos,yPos;   // position of bullet
+	double speed=0.5;       // speed of bullet           
+	
+	public Bullet(int x,int y)
+	{
+		this.xPos=x;
+		this.yPos=y;
+	}
+	
+	public boolean checkEnemy(Zombies zom)              // haven't been used
+	{
+		if(this.xPos==zom.xPos)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public void flying()
+	{
+		xPos+=speed;
+	}
+	
+	public void draw(Graphics g, Image png)                  // load image
+	{
+		g.drawImage(png,(int) this.xPos+20,(int)this.yPos+34);
+		//g.setColor(Color.blue);
+		//g.fillOval(this.xPos, this.yPos, this.dX, this.dY);
+	}
+}
