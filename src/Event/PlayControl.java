80e7b42b5b7dc0b0b44926a53b36eacbad43a590
DuongVu98 <duongvutung1998@gmail.com>
2017-10-28 21:39:19 +0700

    package event including Physic and PlayControl

---
+++ src/Event/PlayControl.java
@@ -0,0 +1,140 @@
+package Event;
+import Characters.*;
+
+import GUI.*;
+import Characters.*;
+
+import org.newdawn.slick.*;
+import java.util.ArrayList;
+
+public class PlayControl {
+	Physic phy =new Physic();
+	
+	private ArrayList<Bullet> bullets=new ArrayList<>();
+	Bullet tempBullet;
+	private ArrayList<Zombies> zombies=new ArrayList<>();
+	Zombies tempZombie;
+	private ArrayList<Sun> sun=new ArrayList<>();
+	Sun tempSun;
+	
+	Main screen;
+	
+	//--------------------------------------BULLET---------------------------------------
+	public void shoot()                        
+	{
+		for(int i=0;i<bullets.size();i++)             //shoot all the bullets in the list
+		{
+			tempBullet=bullets.get(i);
+			
+			if(tempBullet.xPos>screen.WIDTH)       // remove bullets out of screen
+			{
+				removeBullet(tempBullet);
+			}
+			
+			if(phy.Collision(tempBullet, zombies))
+			{
+				removeBullet(tempBullet);
+			}
+			tempBullet.flying();
+		}
+	}
+	
+	public void renderBullet(Graphics g,Image png)              // draw bullets
+	{
+		
+		for(int i=0;i<bullets.size();i++)
+		{
+			tempBullet=bullets.get(i);
+			tempBullet.draw(g,png);
+			//tempBullet.dr
+		}
+	}
+	
+	public void addBullet(Bullet b)
+	{
+		bullets.add(b);
+	}
+	
+	public void removeBullet(Bullet b)
+	{
+		bullets.remove(b);
+	}
+	
+	
+	//--------------------------------------ZOMBIE---------------------------------------
+	public void zomWalk()
+	{
+		for(int i=0;i<zombies.size();i++)             //shoot all the bullets in the list
+		{
+			tempZombie=zombies.get(i);
+			tempZombie.moving();
+			
+			if(tempZombie.xPos<0 || tempZombie.getHp()<=0)   //remove bullet and zombies from screen    
+			{
+				removeZombie(tempZombie);
+			}
+			if(phy.Collision(tempZombie, bullets))
+			{
+				tempZombie.setHp(tempZombie.getHp()-20);
+			}
+		}
+	}
+	
+	public void renderZombie(ArrayList<Image> png,double n)              // draw bullets
+	{
+		
+		for(int i=0;i<zombies.size();i++)
+		{
+			tempZombie=zombies.get(i);
+			tempZombie.draw(png,n);
+			//tempBullet.dr
+		}
+	}
+	
+	public void addZombie(Zombies z)
+	{
+		zombies.add(z);
+	}
+	
+	public void removeZombie(Zombies z)
+	{
+		zombies.remove(z);
+	}
+	//--------------------------------------SUN---------------------------------------
+	
+	public void fall()                        
+	{
+		for(int i=0;i<sun.size();i++)             //shoot all the bullets in the list
+		{
+			tempSun=sun.get(i);
+			tempSun.falling();
+			
+			if(tempSun.xPos>screen.WIDTH)       // remove bullets out of screen
+			{
+				removeSun(tempSun);
+			}
+		}
+	}
+	
+	public void renderSun(Graphics g,Image png)              // draw bullets
+	{
+		
+		for(int i=0;i<sun.size();i++)
+		{
+			tempSun=sun.get(i);
+			tempSun.draw(g,png);
+		}
+	}
+	
+	public void addSun(Sun b)
+	{
+		sun.add(b);
+	}
+	
+	public void removeSun(Sun b)
+	{
+		sun.remove(b);
+	}
+}
+
+
