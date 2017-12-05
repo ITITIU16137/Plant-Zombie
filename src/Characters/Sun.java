6d2d4a47d6738f81b3af12c5b1947720c40ffdca
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2017-12-06 00:12:25 +0700

    Hope this code work well :+1:

--- src/Characters/Sun.java
+++ src/Characters/Sun.java
@@ -4,18 +4,26 @@ package Characters;
 //import org.newdawn.slick.Color;
 import org.newdawn.slick.*;
 public class Sun extends GameObject{
-	
+
 	protected int dX=400,dY=400;       //size of Sun
+	private int randomInt;
 	int speed = 1;      // speed of Sun :D
 
-	
+
 	/*
-	 * 	Constructor 
+	 * 	Constructor
 	 */
 	public Sun(int x,int y)
 	{
 		super(x,y);
+		this.randomInt = (int)(Math.random()*1)+(int)(Math.random()*5);
 	}
+
+	public int getRandomInt()
+	{
+			return randomInt;
+	}
+
 	/*
 	 * 	 Add sun one by one
 	 */
@@ -23,15 +31,15 @@ public class Sun extends GameObject{
 	{
 		if (k == 1)
 		yPos+=speed;
-		else yPos -= 1 ; 
-		
+		else yPos -= 1 ;
+
 	}
 	public void stop() {
-	
+
 		yPos-=speed;
 	}
 	/*
-	 * 	
+	 *
 	 */
 	public void draw(Graphics g, Image png)                  // load image
 	{
