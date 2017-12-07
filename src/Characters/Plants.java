04433f6ea8e8b8dcb9158676e850c52a90e208a7
kingroyalknight <30320962+kingroyalknight@users.noreply.github.com>
2017-12-07 22:37:36 +0700

    sth

--- src/Characters/Plants.java
+++ src/Characters/Plants.java
@@ -5,16 +5,16 @@ import org.newdawn.slick.Image;
 
 public abstract class Plants extends GameObject{
 	
-	public int speed = 1;
+	public double speed = 0.2;
 	protected int hp;
 	//protected int damage;
 	
 	public Plants(int x,int y) 
 	{
-		super();
+		super(x,y);
 	}
 	
-	public int getSpeed()                     { return speed; }
+	public double getSpeed()                     { return speed; }
 	public void setSpeed(int speed)           { this.speed = speed; }
 	public int getHp()                        { return hp; }
 	public void setHp(int hp)                 { this.hp = hp; }
