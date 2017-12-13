113306a38c28b58933a3c413081a52d60945ce40
Vũ Tùng Dương <duongvutung1998@gmail.com>
2017-12-14 01:41:35 +0700

    a new level

---
+++ src/bullet/RocketBullet.java
@@ -0,0 +1,20 @@
+package bullet;
+
+import org.newdawn.slick.Image;
+
+import characters.*;
+
+public class RocketBullet extends Bullet{
+	public static int damage=100;
+	public static String pngName="res/rocket.png";
+	
+	public RocketBullet(double d,double e)
+	{
+		super(d,e);
+	}
+
+	public void draw(Image png)                  // load image
+	{
+		png.draw((int)this.xPos, (int)this.yPos,png.getWidth()/5,png.getHeight()/5);
+	}
+}
